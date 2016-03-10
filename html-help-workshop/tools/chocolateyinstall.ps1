0e0560f076c26ec4213d0f75ccacd9bd8fccdc3a
Derek Wickern <dwickern@gmail.com>
2016-03-10 15:55:19 -0800

    Fix modal dialog after install

--- html-help-workshop/tools/chocolateyinstall.ps1
+++ html-help-workshop/tools/chocolateyinstall.ps1
@@ -8,20 +8,31 @@ $checksum = '53899be5da83419d772d5b97e653da7c'
 $temp_dir = Join-Path $Env:TEMP ([System.Guid]::NewGuid().ToString())
 New-Item -Type Directory -Path $temp_dir | Out-Null
 
+# download auto-extracting zip installer
 $htmlhelp_exe = Join-Path $temp_dir "htmlhelp.exe"
-$htmlhelp_extracted = Join-Path $temp_dir "htmlhelp"
-$htmlhelp_install = Join-Path $htmlhelp_extracted "htmlhelp.inf"
-
-# download and extract
 Get-ChocolateyWebFile $packageName $htmlhelp_exe $url -checksum $checksum
+
+# extract to temporary directory in quiet mode
+$htmlhelp_extracted = Join-Path $temp_dir "htmlhelp"
 Start-ChocolateyProcessAsAdmin "/Q ""/T:$htmlhelp_extracted"" /C" $htmlhelp_exe
 
+# comment out the line which runs hhupd.exe (the automatic updater) after installation
+# the updater will pop up a dialog telling us we are running the latest version already
+$htmlhelp_install = Join-Path $htmlhelp_extracted "htmlhelp.inf"
+$htmlhelp_install_fixed = Join-Path $htmlhelp_extracted "htmlhelp_fixed.inf"
+(Get-Content $htmlhelp_install) -replace '^\"hhupd.exe /C', ';$0' | Set-Content $htmlhelp_install_fixed
+
+# find the 32-bit rundll; always install as 32-bit as there is no 64-bit distribution
 $rundll_exe = Join-Path $Env:SystemRoot "System32\rundll32.exe"
 $rundll_exe6432 = Join-Path $Env:SystemRoot "SysWOW64\rundll32.exe"
 $rundll = @($rundll_exe6432,$rundll_exe) | Where { Test-Path $_ } | Select-Object -First 1
 
-# install from INF; flags = 1 (QUIET), reboot = N
-Start-ChocolateyProcessAsAdmin "advpack.dll,LaunchINFSection ""$htmlhelp_install"",,1,N" $rundll
+# install from INF
+# https://msdn.microsoft.com/en-us/library/gg441316%28v=vs.85%29.aspx
+$section = "" # default entrypoint
+$flags = 3 # LIS_QUIET | LIS_NOGRPCONV
+$reboot = "N"
+Start-ChocolateyProcessAsAdmin "advpack.dll,LaunchINFSection ""$htmlhelp_install_fixed"",$section,$flags,$reboot" $rundll
 
 # cleanup temp directory
 Remove-Item -Path $temp_dir -Recurse
--- html-help-workshop/tools/chocolateyuninstall.ps1
+++ html-help-workshop/tools/chocolateyuninstall.ps1
@@ -2,13 +2,27 @@
 
 $packageName = 'html-help-workshop'
 
-$machine_key   = 'HKLM:\SOFTWARE\Microsoft\HTML Help Workshop'
-$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\HTML Help Workshop'
-$htmlhelp_dir = Get-ItemProperty -Path @($machine_key6432,$machine_key) -ErrorAction SilentlyContinue | Select-Object -ExpandProperty InstallDir -First 1
+# find the uninstaller
+$machine_key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\hhw.exe'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\App Paths\hhw.exe'
+$htmlhelp_dir = Get-ItemProperty -Path @($machine_key6432,$machine_key) -ErrorAction SilentlyContinue |
+  Select-Object -ExpandProperty Path -First 1
+
+if (-not $htmlhelp_dir) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+  return
+}
+
 $htmlhelp_uninstall = Join-Path $htmlhelp_dir "htmlhelp.inf"
 
+# find the 32-bit rundll
 $rundll_exe = Join-Path $Env:SystemRoot "System32\rundll32.exe"
 $rundll_exe6432 = Join-Path $Env:SystemRoot "SysWOW64\rundll32.exe"
 $rundll = @($rundll_exe6432,$rundll_exe) | Where { Test-Path $_ } | Select-Object -First 1
 
-Start-ChocolateyProcessAsAdmin "advpack.dll,LaunchINFSection ""$htmlhelp_uninstall"",,1,N" $rundll
+# uninstall from INF
+# https://msdn.microsoft.com/en-us/library/gg441316%28v=vs.85%29.aspx
+$section = "" # default entrypoint
+$flags = 3 # LIS_QUIET | LIS_NOGRPCONV
+$reboot = "N"
+Start-ChocolateyProcessAsAdmin "advpack.dll,LaunchINFSection ""$htmlhelp_uninstall"",$section,$flags,$reboot" $rundll
